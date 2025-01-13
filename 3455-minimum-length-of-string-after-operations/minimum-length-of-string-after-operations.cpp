45dc5ca14fe128d509b13325009a381c60eff4a2
Prince Pal <palprince3690@gmail.com>
2025-01-13 23:58:31 +0530

    Time: 11 ms (77.02%) | Memory: 32 MB (86.69%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.length();
-        vector<int>freq(26, 0);
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']+=1;
-            if(freq[s[i] -'a']==3){
-                freq[s[i]-'a']-=2;
-            }
-
-        }
         int ans = 0;
+        vector<int>freq(26,0);
+        for(char c : s){
+            freq[c-'a']++;
+        }
         for(int i=0;i<26;i++){
             if(freq[i]==0) continue;
-            ans+=freq[i];
+            if(freq[i]%2==0){
+                ans+=2;
+            }else {
+                ans+=1;
+            }
         }
         return ans;
+        
     }
 };
