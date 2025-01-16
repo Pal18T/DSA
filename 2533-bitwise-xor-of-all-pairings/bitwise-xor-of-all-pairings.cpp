84b38b39b133829ef452abd7c264723366c93db4
Prince Pal <palprince3690@gmail.com>
2025-01-16 22:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 64.3 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        // int n = nums1.size();
+        // int m = nums2.size();
 
-        unordered_map<int, long>mp;
-        for( int &num : nums1){
-            mp[num]+=m;
-        }
-        for( int &num : nums2){
-            mp[num]+=n;
-        }
-        int ans = 0;
-        for(auto &it : mp){
-            int num = it.first;
-            int freq = it.second;
-            if(freq%2 !=0){
-                ans^=num;
+        // unordered_map<int, long>mp;
+        // for( int &num : nums1){
+        //     mp[num]+=m;
+        // }
+        // for( int &num : nums2){
+        //     mp[num]+=n;
+        // }
+        // int ans = 0;
+        // for(auto &it : mp){
+        //     int num = it.first;
+        //     int freq = it.second;
+        //     if(freq%2 !=0){
+        //         ans^=num;
+        //     }
+        // }
+        // return ans;
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int XOR = 0;
+
+        if(m%2 != 0){
+            for(int &num : nums2){
+                XOR^=num;
             }
         }
-        return ans;
+        if(n%2 != 0){
+            for(int &num : nums1){
+                XOR^=num;
+            }
+        }
+        return XOR;
     }
 };
